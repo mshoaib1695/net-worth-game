a12e039b4897fccd35d8a20e1e8013973109cbc7
shoaib095 <shoaib@hashpy.com>
2024-05-24 13:21:40 +0500

    fix

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -4,14 +4,16 @@ import redis from "../../../lib/redisClient";
 import verifyToken from "../../../utils/verifyJWT";
 
 const prisma = new PrismaClient();
-
+export const dynamic = "force-dynamic";
 export async function GET(req: NextRequest) {
   const authorization = req.headers.get("authorization");
   if (!authorization?.split(" ")[1]) {
-    return NextResponse.json(
+    const response = NextResponse.json(
       { error: "Failed to authenticate" },
       { status: 500 }
     );
+    response.headers.set("Cache-Control", "no-store");
+    return response;
   }
   try {
     const token = authorization?.split(" ")[1];
@@ -19,7 +21,9 @@ export async function GET(req: NextRequest) {
     await verifyToken(token);
     const cachedData = await redis.get(cacheKey);
     if (cachedData) {
-      return NextResponse.json({ success: true, data: cachedData });
+      const response = NextResponse.json({ success: true, data: cachedData });
+      response.headers.set("Cache-Control", "no-store");
+      return response;
     }
 
     const users = await prisma.user.findMany({
@@ -43,9 +47,13 @@ export async function GET(req: NextRequest) {
     }));
 
     await redis.set(cacheKey, JSON.stringify(importantData));
-    return NextResponse.json({ success: true, data: importantData });
+    const response = NextResponse.json({ success: true, data: importantData });
+    response.headers.set("Cache-Control", "no-store");
+    return response;
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error }, { status: 500 });
+    const response = NextResponse.json({ error }, { status: 500 });
+    response.headers.set("Cache-Control", "no-store");
+    return response;
   }
 }
