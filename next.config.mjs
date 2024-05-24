3f8e45c24540390cd35bf24390044c14220ebd9d
shoaib095 <shoaib@hashpy.com>
2024-05-24 15:40:11 +0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,25 @@ const nextConfig = {
     async headers() {
         return [
           {
-            source: '/api/*',
+            source: '/api/leaderboard',
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 'no-store, max-age=0',
+              },
+            ],
+          },
+          {
+            source: '/api/updateCache',
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 'no-store, max-age=0',
+              },
+            ],
+          },
+          {
+            source: '/api/increaseMultiplier',
             headers: [
               {
                 key: 'Cache-Control',
